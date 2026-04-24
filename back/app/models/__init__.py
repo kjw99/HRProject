ffbebc08259b3f747a178a25084dba13b4f2b64f
lkk7209 <lkk7209@naver.com>
2026-04-24 10:42:27 +0900

    docs: 파일 위치 수정

--- back/app/models/__init__.py
+++ back/app/models/__init__.py
@@ -1,8 +1,4 @@
-from .category import Category
-from .product import Product
 from database import Base
 from .user import User
-from .wishlist import Wishlist
-from .follow import Follow
 
-__all__ = ["Base", "Category", "Product", "Wishlist", "User", "Follow"]
+__all__ = ["Base", "User"]
--- back/app/schemas/user.py
+++ back/app/schemas/user.py
@@ -2,31 +2,32 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Literal
 
-
-class UserCreate(BaseModel):
+# 공통 필드
+class UserBase(BaseModel):
     user_email: EmailStr
-    password: str
     user_name: str
     role: Literal["admin", "hr", "interviewer"]
 
+# 회원가입 요청
+class UserCreate(UserBase):
+    password: str
 
-class UserResponse(BaseModel):
-    user_id: int
+# 로그인 요청
+class UserLogin(BaseModel):
     user_email: EmailStr
-    user_name: str
-    role: str
+    password: str
+
+# 유저 정보 응답
+class UserResponse(UserBase):
+    user_id: int
     created_at: datetime
 
     class Config:
         from_attributes = True  # SQLAlchemy 모델을 Pydantic으로 변환 허용
 
-
-class UserLogin(BaseModel):
-    user_email: EmailStr
-    password: str
-
-
 class Token(BaseModel):
     access_token: str
-    token_type: str
+    token_type: str = "bearer"
     user_name: str
+    user_id: int
+    role: Literal["admin", "hr", "interviewer"]
