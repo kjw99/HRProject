18251248355ee1d2c3055c4e1c4a4ac5600368d6
sabikakhaliq <sabikakhaliq@gmail.com>
2026-04-24 10:20:35 +0900

    auth

--- back/dependencies/db.py
+++ back/dependencies/db.py
@@ -1,5 +1,32 @@
 from database import AsyncSessionLocal
 
+# async def get_db():
+#     async with AsyncSessionLocal() as session:
+#         yield session
+
+
+from dotenv import load_dotenv
+import os
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL 환경변수가 설정되지 않았습니다.")
+
+# postgresql:// → postgresql+asyncpg://
+ASYNC_DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+
+engine = create_async_engine(ASYNC_DATABASE_URL)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
 async def get_db():
-    async with AsyncSessionLocal() as session:
-        yield session
+    async with AsyncSessionLocal() as db:
+        yield db
+
